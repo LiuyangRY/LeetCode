4f9d923ebbc5ea8496d4868bec699f7f521b54e2
Liuyang <1044457987@qq.com>
2020-12-31 14:14:29 +0800

    1. Main 方法测试性能统计。

--- LeetCodeSolutions/Solutions/Program.cs
+++ LeetCodeSolutions/Solutions/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using LeetCodeSolutions.ArrayProblems;
-using LeetCodeSolutions.Common.Extensions;
-using LeetCodeSolutions.Common.Sort;
+using LeetCodeSolutions.Common.Statistic;
 
 namespace LeetCodeSolutions
 {
@@ -9,16 +7,15 @@ namespace LeetCodeSolutions
     {
         static void Main(string[] args)
         {
-            int[] nums1 = new int[]{1,8,6,2,5,4,8,3,7};
-            HoldTheMostWaterSolution test = new HoldTheMostWaterSolution();
-            // var result = test.MaxArea(nums1);
-            // System.Console.WriteLine($"结果为：{result}");
-            Console.Write("排序前数组：");
-            nums1.Print();
-            ArraySort.BubbleSort(nums1);
-            // ArraySort.QuickSort(nums1, 0, nums1.Length - 1);
-            Console.Write("排序后数组：");
-            nums1.Print();
+            Action action = () => {
+                int[] a = new int[10000];
+                for(int i = 0; i < 10000; i++)
+                {
+                    a[i] = i;
+                }
+                System.Threading.Thread.Sleep(1234);
+            };
+            Statistics.Performance("测试任务", action);
         }
     }
 }
