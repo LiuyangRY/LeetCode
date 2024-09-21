c086a027efa75904ea4fc08e9e24f67054238317
liuyang <1044457987@qq.com>
2024-09-21 17:16:30 +0800

    优化寻找两个正序数组中位数的算法

--- LeetCodeSolutions/Solutions/ArrayProblems/FindMiddleNumberInTowOrderedArraysSolution.cs
+++ LeetCodeSolutions/Solutions/ArrayProblems/FindMiddleNumberInTowOrderedArraysSolution.cs
@@ -14,13 +14,15 @@ namespace LeetCodeSolutions.Solutions.ArrayProblems
             }
             bool isEven = (totalLength & 1) == 0;
             int midIndex = totalLength / 2;
+            var lastIndex = isEven ? midIndex - 1 : -1;
+            #region 处理一个数组长度为0的情况，该情况下可以直接根据索引计算中位数，时间复杂度为O(1)
             if (totalLength == nums1.Length || totalLength == nums2.Length)
             {
                 if (nums1.Length == 0)
                 {
                     if (isEven)
                     {
-                        return (nums2[midIndex] + nums2[midIndex - 1]) / 2d;
+                        return (nums2[midIndex] + nums2[lastIndex]) / 2d;
                     }
                     else
                     {
@@ -31,20 +33,22 @@ namespace LeetCodeSolutions.Solutions.ArrayProblems
                 {
                     if (isEven)
                     {
-                        return (nums1[midIndex] + nums1[midIndex - 1]) / 2d;
+                        return (nums1[midIndex] + nums1[lastIndex]) / 2d;
                     }
                     return nums1[midIndex];
                 }
             }
+            #endregion
             
             if (totalLength == 2)
             {
+                // 处理两个数组长度都是1的情况，时间复杂度是O(1)
                 return (nums1[0] + nums2[0]) / 2d;
             }
+            #region 处理两个数组都不为0的情况，使用两个指针计数跳转的方式找到两个数组合并后的中位数
             int firstIndex = 0, secondIndex = 0, currentValue = 0, lastValue = 0;
             for (int i = 0; i < totalLength; i++)
             {
-                lastValue = currentValue;
                 if (firstIndex >= nums1.Length)
                 {
                     currentValue = nums2[secondIndex++];
@@ -61,6 +65,10 @@ namespace LeetCodeSolutions.Solutions.ArrayProblems
                 {
                     currentValue = nums2[secondIndex++];
                 }
+                if (lastIndex == i)
+                {
+                    lastValue = currentValue;
+                }
                 if(midIndex.Equals(i))
                 {
                     if (isEven)
@@ -73,6 +81,7 @@ namespace LeetCodeSolutions.Solutions.ArrayProblems
                     }
                 }
             }
+            #endregion
             return 0; 
         }
         
@@ -80,8 +89,8 @@ namespace LeetCodeSolutions.Solutions.ArrayProblems
         {
             var action = () =>
             {
-                var nums1 = new int[] { 1, 3 };
-                var nums2 = new int[] { 2, 4 };
+                var nums1 = new int[] { 1, 2 };
+                var nums2 = new int[] { 3, 4 };
                 var result = FindMedianBetweenTwoSortedArrays(nums1, nums2);
                 System.Console.WriteLine($"中位数：{result}");
             };
--- Problem/4.寻找两个正序数组的中位数.cs
+++ Problem/4.寻找两个正序数组的中位数.cs
@@ -14,13 +14,15 @@ public class Solution {
         }
         bool isEven = (totalLength & 1) == 0;
         int midIndex = totalLength / 2;
+        var lastIndex = isEven ? midIndex - 1 : -1;
+        #region 处理一个数组长度为0的情况，该情况下可以直接根据索引计算中位数，时间复杂度为O(1)
         if (totalLength == nums1.Length || totalLength == nums2.Length)
         {
             if (nums1.Length == 0)
             {
                 if (isEven)
                 {
-                    return (nums2[midIndex] + nums2[midIndex - 1]) * 0.5d;
+                    return (nums2[midIndex] + nums2[lastIndex]) / 2d;
                 }
                 else
                 {
@@ -31,19 +33,22 @@ public class Solution {
             {
                 if (isEven)
                 {
-                    return (nums1[midIndex] + nums1[midIndex - 1]) * 0.5d;
+                    return (nums1[midIndex] + nums1[lastIndex]) / 2d;
                 }
                 return nums1[midIndex];
             }
         }
+        #endregion
+        
         if (totalLength == 2)
         {
-            return (nums1[0] + nums2[0]) * 0.5d;
+            // 处理两个数组长度都是1的情况，时间复杂度是O(1)
+            return (nums1[0] + nums2[0]) / 2d;
         }
+        #region 处理两个数组都不为0的情况，使用两个指针计数跳转的方式找到两个数组合并后的中位数
         int firstIndex = 0, secondIndex = 0, currentValue = 0, lastValue = 0;
         for (int i = 0; i < totalLength; i++)
         {
-            lastValue = currentValue;
             if (firstIndex >= nums1.Length)
             {
                 currentValue = nums2[secondIndex++];
@@ -60,11 +65,15 @@ public class Solution {
             {
                 currentValue = nums2[secondIndex++];
             }
+            if (lastIndex == i)
+            {
+                lastValue = currentValue;
+            }
             if(midIndex.Equals(i))
             {
                 if (isEven)
                 {
-                    return (lastValue + currentValue) * 0.5d;
+                    return (lastValue + currentValue) / 2d;
                 }
                 else
                 {
@@ -72,6 +81,7 @@ public class Solution {
                 }
             }
         }
+        #endregion
         return 0; 
     }
 }
